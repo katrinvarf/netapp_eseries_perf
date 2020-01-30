33d03d0e26f5e6596f94e74f7e7cbb7ccf6fdca3
root <root@DESKTOP-8BMP4D9.localdomain>
2020-01-30 18:43:43 +0300

    начало getData

---
+++ getData/getData.go
@@ -0,0 +1,11 @@
+package getData
+
+import(
+	"fmt"
+)
+
+var sections = []string{"Controller", "Drive", "Interface", "System", "Volume"}
+var statisticName =[]string{"readIOps", "writeIOps", "combinedIOps", "readResponseTime", "writeResponseTime", "combinedResponseTime", "readThroughput", "writeThroughput", "combinedThroughput", ""}
+func GetData(){
+	
+}
