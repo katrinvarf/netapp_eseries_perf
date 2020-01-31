c4e26c6f951569dc4f56f0d6a7e86aeb677bbc43
root <root@DESKTOP-8BMP4D9.localdomain>
2020-01-31 19:26:35 +0300

    скелет с названиями метрик

--- getData/getData.go
+++ getData/getData.go
@@ -4,8 +4,33 @@ import(
 	"fmt"
 )
 
-var sections = []string{"Controller", "Drive", "Interface", "System", "Volume"}
-var statisticName =[]string{"readIOps", "writeIOps", "combinedIOps", "readResponseTime", "writeResponseTime", "combinedResponseTime", "readThroughput", "writeThroughput", "combinedThroughput", ""}
+var sections = []string{
+	"Controller",
+	"Drive",
+	"Interface",
+	"System",
+	"Volume"}
+var statisticName =[]string{
+	"readIOps",
+	"writeIOps",
+	"combinedIOps",
+	"readResponseTime",
+	"writeResponseTime",
+	"combinedResponseTime",
+	"readThroughput",
+	"writeThroughput",
+	"combinedThroughput",
+	"maxCpuUtilization",
+	"cpuAvgUtilization",
+	"queueDepthTotal",
+	"queueDepthMax"}
+
+func get_drive_stats(){}
+func get_controller_stats(){}
+func get_interface_stats(){}
+func get_system_stats(){}
+func get_volume_stats(){}
+
 func GetData(){
 	
 }
