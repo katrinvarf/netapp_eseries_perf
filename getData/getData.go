40069a4c14f777b0226cd9a35c83ce407df30ef3
root <root@DESKTOP-8BMP4D9.localdomain>
2020-02-04 12:52:09 +0300

    исправила ошибки

--- getData/getData.go
+++ getData/getData.go
@@ -3,7 +3,10 @@ package getData
 import(
 	"fmt"
 	"net/http"
-	"byte"
+	"io/ioutil"
+	"crypto/tls"
+	"strconv"
+	"encoding/json"
 )
 
 var sections = []string{
@@ -31,88 +34,99 @@ var statisticName =[]string{
 	"unconfiguredSpace",
 	"usedPoolSpace"}
 
-func GetAllData(Username string, Password string, DevicePort int, DeviceAddress string, DeviceName string, DeviceID int){
+func GetAllData(Username string, Password string, DevicePort int, DeviceAddress string, DeviceName string, DeviceID int, GroupName string){
 	for _, section := range sections{
-		go getSection(Username, Password, Section, DevicePort, DeviceAddress)
+		if section=="Controller"{
+			fmt.Println(getSection(Username, Password, section, DevicePort, DeviceAddress, DeviceName, DeviceID, GroupName))
+			break
+		}
 	}
 }
 
-func getSectionPerfData(Username string, Password string, SectionAPI string, DevicePort string, DeviceAddress string, DeviceName string, DeviceID int) []interface{}{
+func getSectionPerfData(Username string, Password string, SectionAPI string, DevicePort int, DeviceAddress string, DeviceName string, DeviceID int) interface{} {
 	urlString := "https://" + DeviceAddress + ":" + strconv.Itoa(DevicePort) + "/devmgr/v2/storage-systems/" + strconv.Itoa(DeviceID) + "/" + SectionAPI
 	tr := &http.Transport{
 		TLSClientConfig : &tls.Config{InsecureSkipVerify: true},
 	}
-	client := &http.Client(Transport: tr)
+	client := &http.Client{Transport: tr}
 	request, _ := http.NewRequest("GET", urlString, nil)
 	request.SetBasicAuth(Username, Password)
 	resp, _ := client.Do(request)
 	defer resp.Body.Close()
-	var buf[]byte
+	var buf []byte
 	buf, _ = ioutil.ReadAll(resp.Body)
 	var raw interface{}
 	json.Unmarshal(buf, &raw)
 	return raw
 }
 
-func getSection(Username string, Password string, Section string, DevicePort int, DeviceAddress string, DeviceName string, DeviceID int, GroupName string){
-	result := make(map[string]float64)
-	var perf_data []interface{}
+func getSection(Username string, Password string, Section string, DevicePort int, DeviceAddress string, DeviceName string, DeviceID int, GroupName string) (result map[string]float64) {
+	result = make(map[string]float64)
+	var perf_data interface{}
 	switch Section{
 		case "Controller":
 			perf_data = getSectionPerfData(Username, Password, "analysed-controller-statistics", DevicePort, DeviceAddress, DeviceName, DeviceID)
 			for _, item := range perf_data.([]interface{}){
-				Name := item.(map[string]interface{})["controllerId"]
-				for _, num := range {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}{
+				Name := item.(map[string]interface{})["controllerId"].(string)
+				for _, num := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}{
 					metricName := statisticName[num]
-					metricValue := item.(map[string]interface{})[statisticName[num]]
-					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] := metricValue
+					metricValue := item.(map[string]interface{})[statisticName[num]].(float64)
+					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] = metricValue
 				}
 			}
+			return
 		case "Drive":
 			perf_data = getSectionPerfData(Username, Password, "analysed-drive-statistics", DevicePort, DeviceAddress, DeviceName, DeviceID)
 			for _, item := range perf_data.([]interface{}){
-				Name := item.(map[string]interface{})["diskId"]
-				for _, num := range {0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12}{
+				Name := item.(map[string]interface{})["diskId"].(string)
+				for _, num := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12}{
 					metricName := statisticName[num]
-					metricValue := item.(map[string]interface{})[statisticName[num]]
-					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] := metricValue
+					metricValue := item.(map[string]interface{})[statisticName[num]].(float64)
+					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] = metricValue
 				}
 			}
+			return
 		case "Interface":
 			perf_data = getSectionPerfData(Username, Password, "analysed-interface-statistics", DevicePort, DeviceAddress, DeviceName, DeviceID)
 			for _, item := range perf_data.([]interface{}){
-				Name := item.(map[string]interface{})["interfaceId"]
-				for _, num := range {0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12}{
+				Name := item.(map[string]interface{})["interfaceId"].(string)
+				for _, num := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12}{
 					metricName := statisticName[num]
-					metricValue := item.(map[string]interface{})[statisticName[num]]
-					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] := metricValue
+					metricValue := item.(map[string]interface{})[statisticName[num]].(float64)
+					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] = metricValue
 				}
 			}
+			return
 		case "System":
 			perf_data = getSectionPerfData(Username, Password, "analysed-system-statistics", DevicePort, DeviceAddress, DeviceName, DeviceID)
 			for _, item := range perf_data.([]interface{}){
-				Name := item.(map[string]interface{})["storageSystemName"]
-				for _, num := range {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}{
+				Name := item.(map[string]interface{})["storageSystemName"].(string)
+				for _, num := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}{
 					metricName := statisticName[num]
-					metricValue := item.(map[string]interface{})[statisticName[num]]
-					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] := metricValue
+					metricValue := item.(map[string]interface{})[statisticName[num]].(float64)
+					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] = metricValue
 				}
 			}
+			return
 		case "Volume":
 			perf_data = getSectionPerfData(Username, Password, "analysed-volume-statistics", DevicePort, DeviceAddress, DeviceName, DeviceID)
 			for _, item := range perf_data.([]interface{}){
-				Name := item.(map[string]interface{})["volumeName"]
-				for _, num := range {0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12}{
+				Name := item.(map[string]interface{})["volumeName"].(string)
+				for _, num := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 12}{
 					metricName := statisticName[num]
-					metricValue := item.(map[string]interface{})[statisticName[num]]
-					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] := metricValue
+					metricValue := item.(map[string]interface{})[statisticName[num]].(float64)
+					result[GroupName + "." + DeviceName + "." + Section + "." + Name + "." + metricName] = metricValue
 				}
 			}
+			return
 		case "Pool":
 			perf_data = getSectionPerfData(Username, Password, "", DevicePort, DeviceAddress, DeviceName, DeviceID)
-			for _, num := range {13, 14, 15}{
+			for _, num := range []int{13, 14, 15}{
 				metricName := statisticName[num]
-				metricValue := item.(map[string]interface{})[statisticName[num]]
-				result[GroupName + "." + DeviceName + "." + Section + "." + metricName] := metricValue
+				metricValue := perf_data.(map[string]interface{})[statisticName[num]].(float64)
+				result[GroupName + "." + DeviceName + "." + Section + "." + metricName] = metricValue
 			}
+			return
+	}
+	return
 }
