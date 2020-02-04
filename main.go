26e53bf9ab36f2f7f96f71856ac76df84d7b7c44
root <root@DESKTOP-8BMP4D9.localdomain>
2020-02-04 17:49:46 +0300

    добавила получение данных, включая проверку доступности адресов

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"./config"
+	"./login"
+	"./getData"
+	//"fmt"
 )
 
 func main(){
@@ -10,4 +13,23 @@ func main(){
 	flag.StringVar(&configPath, "config", "", "Path to the config file")
 	flag.Parse()
 	conf := config.GetConfig(configPath)
+	DeviceID := 1
+	for i:=0; i<len(conf.Groups); i++{
+		for j:=0; j<len(conf.Groups[i].Arrays); j++{
+			DeviceAddress := checkAccessAd(conf.Default.Username, conf.Default.Password, conf.Groups[i].Arrays[j].Address, conf.Default.Port)
+			if DeviceAddress!=""{
+				getData.GetAllData(conf.Default.Username, conf.Default.Password, conf.Default.Port, DeviceAddress, conf.Groups[i].Arrays[j].Name, DeviceID, conf.Groups[i].Groupname)
+			}
+		}
+	}
+}
+
+func checkAccessAd(Username string, Password string, Addresses []string, Port int)(DeviceAddress string){
+	for _, address := range Addresses{
+		if login.Login(Username, Password, address, Port){
+			DeviceAddress = address
+			return
+		}
+	}
+	return
 }
