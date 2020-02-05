8a36a853551ca4efc92d9099cf31de38ce92daa7
root <root@DESKTOP-8BMP4D9.localdomain>
2020-02-05 16:56:45 +0300

    добавила бесконечный цикл и горутину

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"./getData"
 	"time"
 	"runtime"
-	//"fmt"
+	"fmt"
 )
 
 func main(){
@@ -17,21 +17,28 @@ func main(){
 	config.GetConfig(configPath)
 	DeviceID := 1
 	runtime.Gosched()
-	for i:=0; i<len(config.SanPerfConfig.Groups); i++{
-		for j:=0; j<len(config.SanPerfConfig.Groups[i].Arrays); j++{
-			DeviceAddress := checkAccessAd(config.SanPerfConfig.Default.Username, config.SanPerfConfig.Default.Password, config.SanPerfConfig.Groups[i].Arrays[j].Address, config.SanPerfConfig.Default.Port)
-			if DeviceAddress!=""{
-				go worker(config.SanPerfConfig.Default.Username, config.SanPerfConfig.Default.Password, config.SanPerfConfig.Default.Port, DeviceAddress, config.SanPerfConfig.Groups[i].Arrays[j].Name, DeviceID, config.SanPerfConfig.Groups[i].Groupname)
+	runtime.GOMAXPROCS(3)
+	//fmt.Println(runtime.NumCPU())
+	for{
+		//t1 := time.Now().UnixNano()
+		for i:=0; i<len(config.SanPerfConfig.Groups); i++{
+			for j:=0; j<len(config.SanPerfConfig.Groups[i].Arrays); j++{
+				DeviceAddress := checkAccessAd(config.SanPerfConfig.Default.Username, config.SanPerfConfig.Default.Password, config.SanPerfConfig.Groups[i].Arrays[j].Address, config.SanPerfConfig.Default.Port)
+				if DeviceAddress!=""{
+					go worker(config.SanPerfConfig.Default.Username, config.SanPerfConfig.Default.Password, config.SanPerfConfig.Default.Port, DeviceAddress, config.SanPerfConfig.Groups[i].Arrays[j].Name, DeviceID, config.SanPerfConfig.Groups[i].Groupname)
+					fmt.Println("Send data")
+				}
 			}
 		}
+		//t2 := time.Now().UnixNano()
+		time.Sleep(time.Second*time.Duration(config.SanPerfConfig.Default.Interval))
+		fmt.Println("Sleep")
+		//fmt.Println(float64(t2 - t1) / 1000000.0)
 	}
 }
 
 func worker(Username string, Password string, Port int, Address string, DeviceName string, DeviceID int, GroupName string){
-	for{
-		getData.GetAllData(Username, Password, Port, Address, DeviceName, DeviceID, GroupName)
-		time.Sleep(time.Second*time.Duration(config.SanPerfConfig.Default.Interval))
-	}
+	getData.GetAllData(Username, Password, Port, Address, DeviceName, DeviceID, GroupName)
 }
 
 func checkAccessAd(Username string, Password string, Addresses []string, Port int)(DeviceAddress string){
