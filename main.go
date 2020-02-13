bfc26667ce596007ce0288f7433641a1cf4f8fda
root <root@DESKTOP-8BMP4D9.localdomain>
2020-02-13 13:08:50 +0300

    Добавила вариант записи логов в один источник

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/snowzach/rotatefilehook"
 	"os"
+	"io"
 )
 
 func main(){
@@ -49,25 +50,44 @@ func main(){
 func initLogger(log *logrus.Logger) (err error){
 	logLevels := map[string]logrus.Level{"trace": logrus.TraceLevel, "debug": logrus.DebugLevel, "info": logrus.InfoLevel, "warn": logrus.WarnLevel, "error": logrus.ErrorLevel, "fatal": logrus.FatalLevel, "panic": logrus.PanicLevel}
 	formatters := map[string]logrus.Formatter{"json": &logrus.JSONFormatter{TimestampFormat: "02-01-2006 15:04:05"}, "text": &logrus.TextFormatter{TimestampFormat: "02-01-2006 15:04:05", FullTimestamp: true}}
-	log.SetLevel(logLevels[config.SanPerfConfig.Loggers[0].Level])
-	log.SetOutput(os.Stdout)
-	log.SetFormatter(formatters[config.SanPerfConfig.Loggers[0].Encoding])
 
-	var rotateFileHook logrus.Hook
-	rotateFileHook, err = rotatefilehook.NewRotateFileHook(rotatefilehook.RotateFileConfig{
-		Filename: config.SanPerfConfig.Loggers[1].File,
-		MaxSize: 50, //megabytes
-		MaxBackups: 3,
-		MaxAge: 28, //days
-		Level: logLevels[config.SanPerfConfig.Loggers[1].Level],
-		Formatter: formatters[config.SanPerfConfig.Loggers[1].Encoding],
-	})
+	if len(config.SanPerfConfig.Loggers)==2{
+		setValuesLogrus(log, logLevels[config.SanPerfConfig.Loggers[0].Level], os.Stdout, formatters[config.SanPerfConfig.Loggers[0].Encoding])
+		var rotateFileHook logrus.Hook
+		rotateFileHook, err = rotatefilehook.NewRotateFileHook(rotatefilehook.RotateFileConfig{
+			Filename: config.SanPerfConfig.Loggers[1].File,
+			MaxSize: 50, //megabytes
+			MaxBackups: 3,
+			MaxAge: 28, //days
+			Level: logLevels[config.SanPerfConfig.Loggers[1].Level],
+			Formatter: formatters[config.SanPerfConfig.Loggers[1].Encoding],
+		})
+		log.AddHook(rotateFileHook)
+	} else if len(config.SanPerfConfig.Loggers)==1{
+		if config.SanPerfConfig.Loggers[0].File=="stdout"{
+			setValuesLogrus(log, logLevels[config.SanPerfConfig.Loggers[0].Level], os.Stdout, formatters[config.SanPerfConfig.Loggers[0].Encoding])
+		} else{
+			var file io.Writer
+			file, err = os.OpenFile("carbonapi.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			if err!=nil{
+				log.Warning("Failed to initialize log file: Error: ", err)
+				return
+			}
+			setValuesLogrus(log, logLevels[config.SanPerfConfig.Loggers[0].Level], file, formatters[config.SanPerfConfig.Loggers[0].Encoding])
+		}
+	}
+
 	if err!=nil{
 		log.Warning("Failed to initialize file rotate hook: Error: ", err)
 		return
 	}
-	log.AddHook(rotateFileHook)
 	return nil
+}
+
+func setValuesLogrus(log *logrus.Logger, level logrus.Level, output io.Writer, formatter logrus.Formatter){
+	log.SetLevel(level)
+	log.SetOutput(output)
+	log.SetFormatter(formatter)
 }
 
 func checkAccessAd(log *logrus.Logger, Username string, Password string, Addresses []string, Port int)(DeviceAddress string, err error){
